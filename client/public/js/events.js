76f134a1afa3ca5d9da09b5fa5bb4a4f0912de5b
muitinyau <nuitinyau@gmail.com>
2023-04-09 18:57:22 +0800

    change

--- client/public/js/events.js
+++ client/public/js/events.js
@@ -3,6 +3,33 @@ function droplist() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function shownav() {
+  var x = document.getElementById("navbar");
+  var y = document.getElementById("dropdown");
+  var z = document.getElementById("myDropdown");
+  var a = document.getElementById("dropbtn");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+  if (y.className === "dropdown") {
+    y.className += " responsive";
+  } else {
+    y.className = "dropdown";
+  }
+  if (z.className === "dropdown-content") {
+    z.className += " responsive";
+  } else {
+    z.className = "dropdown-content";
+  }
+  if (a.className === "dropbtn") {
+    a.className += " responsive";
+  } else {
+    a.className = "dropbtn";
+  }
+}
+
 function copyToClipboard() {
   var copyText = document.getElementById("txid").innerHTML;
   navigator.clipboard.writeText(copyText);
@@ -11,7 +38,7 @@ function copyToClipboard() {
 function disablebtn() {
   const button = document.querySelector('input[type="submit"]');
 
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     button.disabled = true;
   });
 }
@@ -22,15 +49,16 @@ function requireLogin(req, res, next) {
   } else {
     // redirect to login page
     res.redirect("/login");
-
   }
 }
 
 var Web3 = require("web3");
 const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:8545");
 // Define contract address
-const fs = require('fs');
-const abipath = JSON.parse(fs.readFileSync('../build/contracts/evoting.json', 'utf8'));
+const fs = require("fs");
+const abipath = JSON.parse(
+  fs.readFileSync("../build/contracts/evoting.json", "utf8")
+);
 const smartcontractaddress = abipath.networks[5777].address;
 
 const abi = abipath.abi;
@@ -38,4 +66,4 @@ const abi = abipath.abi;
 // Load contract
 const contract = new web3.eth.Contract(abi, smartcontractaddress);
 
-module.exports = {web3, contract, requireLogin};
+module.exports = { web3, contract, requireLogin };
